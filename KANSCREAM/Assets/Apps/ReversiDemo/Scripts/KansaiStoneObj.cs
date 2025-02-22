f6e2fa22e3c77f2ede0d70ac7f46fc7aa3620e32
kdix-23-0240 <2312110240g@kindai.ac.jp>
2025-02-23 01:46:40 +0900

    ゲーム中のBGM実装

--- KANSCREAM/Assets/Apps/ReversiDemo/Scripts/KansaiStoneObj.cs
+++ KANSCREAM/Assets/Apps/ReversiDemo/Scripts/KansaiStoneObj.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
+// using DG.Tweening;
+// using Cysharp.Threading.Tasks;
+// using System.Threading;
+
 
 public class KansaiStoneObj : MonoBehaviour
 {
+    [SerializeField] private AudioSource sound;
     void Start()
     {
+        sound = GetComponent<AudioSource>();
         this.transform.rotation = Quaternion.Euler(-90, 180, 0);
     }
     public void SetState(OthelloSystem.SpriteState state)
     {
-        // var active = state != OthelloSystem.SpriteState.NONE && state != OthelloSystem.SpriteState.KANTO;
-        // gameObject.SetActive(active);
         var isActived = state != OthelloSystem.SpriteState.NONE && state != OthelloSystem.SpriteState.KANTO;
         gameObject.SetActive(isActived);
     }
--- KANSCREAM/Assets/Apps/ReversiDemo/Scripts/KantoStoneObj.cs
+++ KANSCREAM/Assets/Apps/ReversiDemo/Scripts/KantoStoneObj.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
+// using DG.Tweening;
+// using Cysharp.Threading.Tasks;
+// using System.Threading;
 
 public class KantoStoneObj : MonoBehaviour
 {
+    [SerializeField] private AudioSource sound;
+    // private Sequence sequence;
     void Start()
     {
+        sound = GetComponent<AudioSource>();
         this.transform.rotation = Quaternion.Euler(-90, 180, 0);
     }
     public void SetState(OthelloSystem.SpriteState state)
     {
-        // var active = state != OthelloSystem.SpriteState.NONE && state != OthelloSystem.SpriteState.KANSAI;
-        // gameObject.SetActive(active);
         var isActived = state != OthelloSystem.SpriteState.NONE && state != OthelloSystem.SpriteState.KANSAI;
         gameObject.SetActive(isActived);
     }
